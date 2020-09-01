b33946d52c27f78daa3e567d9cc277e4bd99b8ef
D3VSJ <D3VSJ@protonmail.com>
2020-09-01 15:51:38 +0100

    Removed BetterDiscordPanel v1.0.0 and added files for v2.0.0.
    BetterDiscordPanel v2.0.0 is here!
    
    This update now comes with a whole new design for the Panel, better support, and more!

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * @file app.js
+ * @author Sanjay Sunil (a.k.a D3VSJ)
+ * @license GPL-3.0
+ */
+
+! function (a) {
+    "use strict";
+    a(".dropdown-menu a.dropdown-toggle").on("click", function (t) {
+        return a(this).next().hasClass("show") || a(this).parents(".dropdown-menu").first().find(".show").removeClass("show"), a(this).next(".dropdown-menu").toggleClass("show"), !1
+    }), a(function () {
+        a('[data-toggle="tooltip"]').tooltip()
+    }), a(function () {
+        a('[data-toggle="popover"]').popover()
+    }), a("#light-dark").on("click", function (t) {
+        "disabled" !== a("#bootstrap-style").attr("disabled") ? (a("#bootstrap-dark-style").attr("disabled", !1), a("#bootstrap-style").attr("disabled", !0), a("#app-dark-style").attr("disabled", !1), a("#app-style").attr("disabled", !0)) : (a("#bootstrap-dark-style").attr("disabled", !0), a("#bootstrap-style").attr("disabled", !1), a("#app-dark-style").attr("disabled", !0), a("#app-style").attr("disabled", !1))
+    }), Waves.init()
+}(jQuery);
---
+++ assets/js/pages/index.init.js
@@ -0,0 +1,30 @@
+/**
+ * @file index.init.js
+ * @author Sanjay Sunil (a.k.a D3VSJ)
+ * @license GPL-3.0
+ */
+
+$(document).ready(function () {
+    $(".popup-img").magnificPopup({
+        type: "image",
+        closeOnContentClick: !0,
+        mainClass: "mfp-img-mobile",
+        image: {
+            verticalFit: !0
+        }
+    }), $("#user-status-carousel").owlCarousel({
+        items: 4,
+        loop: !1,
+        margin: 16,
+        nav: !1,
+        dots: !1
+    }), $("#user-profile-hide").click(function () {
+        $(".user-profile-sidebar").hide()
+    }), $(".user-profile-show").click(function () {
+        $(".user-profile-sidebar").show()
+    }), $(".channels").click(function () {
+        $(".user-chat").addClass("user-chat-show")
+    }), $(".user-chat-remove").click(function () {
+        $(".user-chat").removeClass("user-chat-show")
+    })
+});
