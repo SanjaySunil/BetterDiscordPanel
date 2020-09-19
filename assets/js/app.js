9f515cfbfaeead85b502fbafe5052f23be1643c3
D3VSJ <D3VSJ@protonmail.com>
2020-09-19 14:14:20 +0100

    Updates to Index.

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,12 @@
     }), Waves.init()
 }(jQuery);
 
+/**
+ * @file app.js
+ * @author Sanjay Sunil (a.k.a D3VSJ)
+ * @license GPL-3.0
+ */
+
 $(document).ready(function () { 
     new Noty({
         type: 'info',
