160ef8b56f0ce7cb5e0b7a64eec48f1b36da9441
Sanjay Sunil <sanjaysunil@protonmail.com>
2021-02-07 20:11:48 +0000

    Removed unused code.

--- assets/js/content.js
+++ assets/js/content.js
@@ -4,22 +4,14 @@
  * @license GPL-3.0
  */
 
-// Text
-$("#animCheck").html(localeFile.text.scrollCheck);
 channelName.html(`${localeFile.text.channelNameLabel}`);
 
-// Headings
 guildName.html(
 	`<img class="avatarIMG" src="" alt=""> ${localeFile.headings.guildName}`
 );
 guildNameNoPic.html(`${localeFile.headings.guildName}`);
-$("#autoScrollHead").html(localeFile.headings.autoScroll);
-$("#lastMessagesHead").html(
-	`<img class="avatarIMG" src='./img/icon/clock.png' alt="clock"> ${localeFile.headings.lastMessages}`
-);
 $("#last").html(localeFile.headings.lastMessages);
 
-// Buttons
 refreshToken.html(
 	`${localeFile.buttons.editToken}<i class="ri-logout-circle-r-line float-right text-muted"></i>`
 );
@@ -28,11 +20,4 @@ $("#language").html(`${localeFile.buttons.changeLanguage}`);
 leaveGuild.html(`${localeFile.buttons.leave}`);
 inviteBtn.html(`${localeFile.buttons.invite}`);
 send.html(`${localeFile.buttons.send}`);
-clearChat.html(`${localeFile.buttons.clearLastMessages}`);
 
-// Formatting
-$("#bold").attr("title", localeFile.formatting.bold);
-$("#emphasis").attr("title", localeFile.formatting.emphasis);
-$("#underline").attr("title", localeFile.formatting.underline);
-$("#strike").attr("title", localeFile.formatting.strike);
-$("#clear").attr("title", localeFile.formatting.clear);
