79453b4838f2a447b1b5c8e7e034c56fcc8a1793
Sanjay Sunil <sanjaysunil@protonmail.com>
2021-03-01 10:31:42 +0000

    Moved betterDiscordConsole function to log.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,24 +73,3 @@
   }
   checkTheme();
 })(jQuery);
-
-function betterDiscordConsole(message, color) {
-  color = color || 'black';
-  switch (color) {
-    case 'success':
-      color = 'Green';
-      break;
-    case 'info':
-      color = 'DodgerBlue';
-      break;
-    case 'error':
-      color = 'Red';
-      break;
-    case 'warning':
-      color = 'Orange';
-      break;
-    default:
-      color = color;
-  }
-  console.log('%c' + message, 'color:' + color);
-}
