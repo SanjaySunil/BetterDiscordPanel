42f1e0624e66fd1cf9118a09a3dbaf65fed99669
Anokino <nkoloiz58@gmail.com>
2022-07-01 23:10:43 +0200

    Update locales.js

--- assets/js/locales.js
+++ assets/js/locales.js
@@ -5,179 +5,267 @@
  */
 
 const locales = {
-  en: {
-    'langCode': 'en',
-    'language': 'English',
-    // ========================= PANEL =========================
-    'status': {
-      loading: 'Loading ...',
+    en: {
+        'langCode': 'en',
+        'language': 'English',
+        // ========================= PANEL =========================
+        'status': {
+            loading: 'Loading ...',
+        },
+        'headings': {
+            profile: 'Discord Bot',
+            guildName: 'Chats',
+            settings: 'Settings',
+        },
+        'sub-headings': {
+            about: 'About',
+            tools: 'Tools',
+            info: 'Info',
+        },
+        'navbar': {
+            profileNav: 'Profile',
+            chatNav: 'Chat',
+            lastMessagesNav: 'Last Messages',
+            infoNav: 'Chat / Server Info',
+            settingsNav: 'Settings',
+            lightDark: 'Light / Dark Mode',
+            logoutTitle: 'Logout',
+        },
+        'text': {
+            channelNameLabel: 'Chat',
+            fileSent: 'sent some file(s)',
+            attachmentTxt: 'Attachments',
+            privateMessages: 'DM',
+            profileSection: {
+                name: 'Name',
+                discrim: 'Discrim',
+                id: 'ID',
+                guilds: 'Guilds',
+                createdAt: 'Created At',
+            },
+            settingsSection: {
+                createBotInvite: 'Create Bot Invite',
+                eval: 'EVAL',
+                languages: 'Languages',
+            },
+        },
+        'buttons': {
+            clearLastMessages: 'Clear Last Messages',
+            editToken: 'Logout',
+            refreshChat: 'Refresh chat',
+            send: 'Send',
+            leave: 'Leave server',
+            invite: 'Invite',
+        },
+        // ========================= DISCORD =========================
+        'infos': {
+            owner: 'Owner',
+            members: 'Members',
+            vChannels: 'Channels (voice)',
+            tChannels: 'Channels (text)',
+            roles: 'Roles',
+            channels: 'Channels',
+            emojis: 'Emojis',
+        },
+        'errors': {
+            error: 'ERROR',
+            dm: 'ERROR : DM',
+            emptyMsg: 'Empty Message!',
+            missingPermissions: 'Missing Permissions!',
+        },
+        'token': {
+            confirmation: 'Are you sure you want to logout?',
+            invalid: 'Token is Invalid. Please try again.',
+        },
+        'messageType': {
+            serverJoin: 'joined the server.',
+            pin: 'pinned a message.',
+            channelNews: 'added a remote channel news.',
+            boost: 'boosted the server.',
+        },
+        'fileType': {
+            unknown: 'unknown',
+            img: 'image',
+            doc: 'document',
+            pdf: 'pdf',
+            video: 'video',
+            audio: 'audio',
+        },
     },
-    'headings': {
-      profile: 'Discord Bot',
-      guildName: 'Chats',
-      settings: 'Settings',
-    },
-    'sub-headings': {
-      about: 'About',
-      tools: 'Tools',
-      info: 'Info',
-    },
-    'navbar': {
-      profileNav: 'Profile',
-      chatNav: 'Chat',
-      lastMessagesNav: 'Last Messages',
-      infoNav: 'Chat / Server Info',
-      settingsNav: 'Settings',
-      lightDark: 'Light / Dark Mode',
-      logoutTitle: 'Logout',
-    },
-    'text': {
-      channelNameLabel: 'Chat',
-      fileSent: 'sent some file(s)',
-      attachmentTxt: 'Attachments',
-      privateMessages: 'DM',
-      profileSection: {
-        name: 'Name',
-        discrim: 'Discrim',
-        id: 'ID',
-        guilds: 'Guilds',
-        createdAt: 'Created At',
-      },
-      settingsSection: {
-        createBotInvite: 'Create Bot Invite',
-        eval: 'EVAL',
-        languages: 'Languages',
-      },
-    },
-    'buttons': {
-      clearLastMessages: 'Clear Last Messages',
-      editToken: 'Logout',
-      refreshChat: 'Refresh chat',
-      send: 'Send',
-      leave: 'Leave server',
-      invite: 'Invite',
-    },
-    // ========================= DISCORD =========================
-    'infos': {
-      owner: 'Owner',
-      members: 'Members',
-      vChannels: 'Channels (voice)',
-      tChannels: 'Channels (text)',
-      roles: 'Roles',
-      channels: 'Channels',
-      emojis: 'Emojis',
-    },
-    'errors': {
-      error: 'ERROR',
-      dm: 'ERROR : DM',
-      emptyMsg: 'Empty Message!',
-      missingPermissions: 'Missing Permissions!',
-    },
-    'token': {
-      confirmation: 'Are you sure you want to logout?',
-      invalid: 'Token is Invalid. Please try again.',
-    },
-    'messageType': {
-      serverJoin: 'joined the server.',
-      pin: 'pinned a message.',
-      channelNews: 'added a remote channel news.',
-      boost: 'boosted the server.',
-    },
-    'fileType': {
-      unknown: 'unknown',
-      img: 'image',
-      doc: 'document',
-      pdf: 'pdf',
-      video: 'video',
-      audio: 'audio',
-    },
-  },
 
-  id: {
-    'langCode': 'id',
-    'language': 'Indonesia',
-    // ========================= PANEL =========================
-    'status': {
-      loading: 'Memuat ...',
+    id: {
+        'langCode': 'id',
+        'language': 'Indonesia',
+        // ========================= PANEL =========================
+        'status': {
+            loading: 'Memuat ...',
+        },
+        'headings': {
+            profile: 'Bot Discord',
+            guildName: 'Obrolan',
+            settings: 'Pengaturan',
+        },
+        'sub-headings': {
+            about: 'Tentang',
+            tools: 'Peralatan',
+            info: 'Info',
+        },
+        'navbar': {
+            profileNav: 'Profil',
+            chatNav: 'Pesan',
+            lastMessagesNav: 'Pesan Terakhir',
+            infoNav: 'Pesan / Info Server',
+            settingsNav: 'Pengaturan',
+            lightDark: 'Mode Terang / Gelap',
+            logoutTitle: 'Keluar',
+        },
+        'text': {
+            channelNameLabel: 'Pesan',
+            fileSent: 'Mengirim kan sebuah file',
+            attachmentTxt: 'Lampiran',
+            privateMessages: 'Pesan langsung',
+            profileSection: {
+                name: 'Nama',
+                discrim: 'Diskriminasi',
+                id: 'ID',
+                guilds: 'Persekutuan',
+                createdAt: 'Dibuat saat',
+            },
+            settingsSection: {
+                createBotInvite: 'Buat undangan bot',
+                eval: 'EVAL',
+                languages: 'Bahasa',
+            },
+        },
+        'buttons': {
+            clearLastMessages: 'Bersihkan pesan terakhir',
+            editToken: 'Keluar',
+            refreshChat: 'Menyegarkan pesan',
+            send: 'Kirim',
+            leave: 'Keluar Server',
+            invite: 'Undangan',
+        },
+        // ========================= DISCORD =========================
+        'infos': {
+            owner: 'Pemilik',
+            members: 'Anggota',
+            vChannels: 'Saluran (suara)',
+            tChannels: 'Saluran (teks)',
+            roles: 'Peran',
+            channels: 'Saluran',
+            emojis: 'Emoji',
+        },
+        'errors': {
+            error: 'Kesalahan',
+            dm: 'Kesalahan : Pesan Langsung',
+            emptyMsg: 'Pesan Kosong.',
+            missingPermissions: 'Tidak Mempunyai Izin!',
+        },
+        'token': {
+            confirmation: 'Apakah kamu yakin ingin keluar?',
+            invalid: 'Token tidak Valid. Silakan coba lagi.',
+        },
+        'messageType': {
+            serverJoin: 'Bergabung dengan server ini.',
+            pin: 'Pesan yang disematkan',
+            channelNews: 'Menambahkan berita saluran jarak jauh.',
+            boost: 'Meningkatkan server ini.',
+        },
+        'fileType': {
+            unknown: 'Tidak dikenal',
+            img: 'Foto',
+            doc: 'Dokumen',
+            pdf: 'pdf',
+            video: 'video',
+            audio: 'audio',
+        },
     },
-    'headings': {
-      profile: 'Bot Discord',
-      guildName: 'Obrolan',
-      settings: 'Pengaturan',
+
+    fr: {
+        langCode: "fr",
+        language: "Français",
+        // ========================= PANEL =========================
+        status: {
+            loading: "Chargement ...",
+        },
+        headings: {
+            profile: "Bot Discord",
+            guildName: "Chats",
+            settings: "Paramètres"
+        },
+        "sub-headings": {
+            about: "A propos",
+            tools: "Outils",
+            info: "Infos",
+        },
+        navbar: {
+            profileNav: "Profil",
+            chatNav: "Chat",
+            lastMessagesNav: "Derniers messages",
+            infoNav: "Chat / Info Serveur",
+            settingsNav: "Paramètres",
+            lightDark: "Mode Sombre / Clair",
+            logoutTitle: "Déconnexion",
+        },
+        text: {
+            channelNameLabel: "Chat",
+            fileSent: "à envoyé un fichiers(s)",
+            attachmentTxt: "Fichier joint",
+            privateMessages: "MP",
+            profileSection: {
+                name: "Pseudo :",
+                discrim: "Tag :",
+                id: "ID :",
+                guilds: "Nombre de serveurs :",
+                createdAt: "Créé le :"
+            },
+            settingsSection: {
+                createBotInvite: "Créer une invitation du bot",
+                eval: "EVALUER",
+                languages: "Languages",
+            },
+        },
+        buttons: {
+            clearLastMessages: "Effacer les derniers messages",
+            editToken: "Se déconnecter",
+            refreshChat: "Rafraîchir le chat",
+            send: "Envoyer",
+            leave: "Quitter le serveur",
+            invite: "Inviter",
+        },
+        // ========================= DISCORD =========================
+        infos: {
+            owner: "Propriétaire",
+            members: "Membres",
+            vChannels: "Salons (vocaux)",
+            tChannels: "Salons (textuels)",
+            roles: "Rôles",
+            channels: "Salons",
+            emojis: "Emojis",
+        },
+        errors: {
+            error: "ERREUR",
+            dm: "ERREUR : MP",
+            emptyMsg: "Message vide!",
+            missingPermissions: "Autorisations manquantes!",
+        },
+        token: {
+            confirmation: "Êtes-vous sûr de vouloir vous déconnecter?",
+            invalid: "Le token n'est pas valide. Veuillez réessayer.",
+        },
+        messageType: {
+            serverJoin: "a rejoint le serveur.",
+            pin: "a épinglé un message.",
+            channelNews: "a ajouté un salon de news.",
+            boost: "a boosté le serveur.",
+        },
+        fileType: {
+            unknown: "inconnu",
+            img: "image",
+            doc: "document",
+            pdf: "pdf",
+            video: "video",
+            audio: "audio",
+        },
     },
-    'sub-headings': {
-      about: 'Tentang',
-      tools: 'Peralatan',
-      info: 'Info',
-    },
-    'navbar': {
-      profileNav: 'Profil',
-      chatNav: 'Pesan',
-      lastMessagesNav: 'Pesan Terakhir',
-      infoNav: 'Pesan / Info Server',
-      settingsNav: 'Pengaturan',
-      lightDark: 'Mode Terang / Gelap',
-      logoutTitle: 'Keluar',
-    },
-    'text': {
-      channelNameLabel: 'Pesan',
-      fileSent: 'Mengirim kan sebuah file',
-      attachmentTxt: 'Lampiran',
-      privateMessages: 'Pesan langsung',
-      profileSection: {
-        name: 'Nama',
-        discrim: 'Diskriminasi',
-        id: 'ID',
-        guilds: 'Persekutuan',
-        createdAt: 'Dibuat saat',
-      },
-      settingsSection: {
-        createBotInvite: 'Buat undangan bot',
-        eval: 'EVAL',
-        languages: 'Bahasa',
-      },
-    },
-    'buttons': {
-      clearLastMessages: 'Bersihkan pesan terakhir',
-      editToken: 'Keluar',
-      refreshChat: 'Menyegarkan pesan',
-      send: 'Kirim',
-      leave: 'Keluar Server',
-      invite: 'Undangan',
-    },
-    // ========================= DISCORD =========================
-    'infos': {
-      owner: 'Pemilik',
-      members: 'Anggota',
-      vChannels: 'Saluran (suara)',
-      tChannels: 'Saluran (teks)',
-      roles: 'Peran',
-      channels: 'Saluran',
-      emojis: 'Emoji',
-    },
-    'errors': {
-      error: 'Kesalahan',
-      dm: 'Kesalahan : Pesan Langsung',
-      emptyMsg: 'Pesan Kosong.',
-      missingPermissions: 'Tidak Mempunyai Izin!',
-    },
-    'token': {
-      confirmation: 'Apakah kamu yakin ingin keluar?',
-      invalid: 'Token tidak Valid. Silakan coba lagi.',
-    },
-    'messageType': {
-      serverJoin: 'Bergabung dengan server ini.',
-      pin: 'Pesan yang disematkan',
-      channelNews: 'Menambahkan berita saluran jarak jauh.',
-      boost: 'Meningkatkan server ini.',
-    },
-    'fileType': {
-      unknown: 'Tidak dikenal',
-      img: 'Foto',
-      doc: 'Dokumen',
-      pdf: 'pdf',
-      video: 'video',
-      audio: 'audio',
-    },
-  },
 };
