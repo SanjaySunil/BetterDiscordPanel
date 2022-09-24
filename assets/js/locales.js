1f308a833bd141b1f5b779814e22e24e3b1ba406
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-09-24 22:55:15 +0800

    Update locales.js

--- assets/js/locales.js
+++ assets/js/locales.js
@@ -180,4 +180,91 @@ const locales = {
       audio: 'audio',
     },
   },
+   cn: {
+    'langCode': 'zh-CN',
+    'language': 'Simplified Chinese',
+    // ========================= PANEL =========================
+    'status': {
+      loading: '正在进行当中 ...',
+    },
+    'headings': {
+      profile: 'Discord 机器人',
+      guildName: '聊天室',
+      settings: '设定',
+    },
+    'sub-headings': {
+      about: '关于',
+      tools: '用具',
+      info: '资料',
+    },
+    'navbar': {
+      profileNav: '账号',
+      chatNav: '聊天',
+      lastMessagesNav: '最新的讯息',
+      infoNav: '关于聊天 / 服务器',
+      settingsNav: '设定',
+      lightDark: '黑暗 / 白天模式',
+      logoutTitle: '登出',
+    },
+    'text': {
+      channelNameLabel: '讯息',
+      fileSent: '上载文件',
+      attachmentTxt: '附件',
+      privateMessages: '私讯',
+      profileSection: {
+        name: '用户名',
+        discrim: 'Tag',
+        id: '身份号码',
+        guilds: '服务器',
+        createdAt: '创造',
+      },
+      settingsSection: {
+        createBotInvite: '创造邀请机器人链接',
+        eval: '程序',
+        languages: '语言',
+      },
+    },
+    'buttons': {
+      clearLastMessages: '删除最新讯息',
+      editToken: '登出',
+      refreshChat: '更新聊天室',
+      send: '发送',
+      leave: '退出服务器',
+      invite: '邀请',
+    },
+    // ========================= DISCORD =========================
+    'infos': {
+      owner: '主人',
+      members: '会员',
+      vChannels: '语音室',
+      tChannels: '聊天室',
+      roles: '岗位',
+      channels: '聊天及语音室',
+      emojis: 'Emojis',
+    },
+    'errors': {
+      error: 'Error',
+      dm: 'ERROR : 私讯',
+      emptyMsg: '空的讯息!',
+      missingPermissions: '缺乏权利!',
+    },
+    'token': {
+      confirmation: '确定登出?',
+      invalid: '账号的秘号错误，请再试过。',
+    },
+    'messageType': {
+      serverJoin: '加入了服务器。',
+      pin: '标注信息',
+      channelNews: '开了公告室',
+      boost: '为服务器助力',
+    },
+    'fileType': {
+      unknown: '未知',
+      img: '照片',
+      doc: '文件',
+      pdf: 'pdf',
+      video: '影片',
+      audio: '语音',
+    },
+  },
 };
